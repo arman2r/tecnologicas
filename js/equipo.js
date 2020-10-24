8f473a7db73a1283a5167525770ea1dd500adc53
Armando Rubio <arubio@qvision.com.co>
2020-10-24 15:04:39 -0500

    equipo de trabajo

---
+++ js/equipo.js
@@ -0,0 +1,23 @@
+var slider = tns({
+    container: '.equipo',
+    slideBy: 1,
+    mouseDrag: true,
+    swipeAngle: false,
+    speed: 400,
+    items: 3,
+    nested: "inner",
+    edgePadding: 20,
+    nav: false,
+    controlsText: ['<span class="material-icons">keyboard_arrow_left</span>', '<span class="material-icons">keyboard_arrow_right</span>'],
+    responsive: {
+        300: {
+            items: 1,
+        },
+        600: {
+            items: 2,
+        },
+        1024: {
+            items: 3,
+        }
+    }
+});
