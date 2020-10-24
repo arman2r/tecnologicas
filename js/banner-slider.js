5b517f431a2c330a272f55df33faaf1ea81004a1
Armando Rubio <arubio@qvision.com.co>
2020-10-24 13:28:03 -0500

    formulario home

--- js/banner-slider.js
+++ js/banner-slider.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.slider');
     var instances = M.Slider.init(elems);
+
+    var select = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(select);
 });
